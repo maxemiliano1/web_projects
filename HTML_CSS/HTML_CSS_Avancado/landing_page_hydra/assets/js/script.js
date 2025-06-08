4d1b6f20849d25e219706087c1acb09cfadcf0be
Max Emiliano <maxemilio13@hotmail.com>
2025-06-08 18:23:30 -0300

    adicionado section 2

--- HTML_CSS/HTML_CSS_Avancado/landing_page_hydra/assets/js/script.js
+++ HTML_CSS/HTML_CSS_Avancado/landing_page_hydra/assets/js/script.js
@@ -8,5 +8,24 @@ window.onload = function () {
         menuList.classList.toggle("ativo");
         buttonHeader.classList.toggle("ativo");
     });
-  };
+};
 
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const carrossel = document.querySelector('.carrossel-container');
+
+// Avança 1 item (scroll para direita)
+nextBtn.addEventListener('click', () => {
+    carrossel.scrollBy({
+        left: 240, // valor do item + espaçamento
+        behavior: 'smooth'
+    });
+});
+
+// Volta 1 item (scroll para esquerda)
+prevBtn.addEventListener('click', () => {
+    carrossel.scrollBy({
+        left: -240,
+        behavior: 'smooth'
+    });
+});
